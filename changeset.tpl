b01431c6db71ad4e6a0db9e927593c560ace7369
Alexander Tsebenko <d00020@mail.ru>
2020-12-01 22:56:25 +0700

    MVP

---
+++ changeset.tpl
@@ -0,0 +1,52 @@
+    <changeSet author="{{ffAuthor}}" id="insert_FF_{{ffNumber}}">
+        <preConditions onFail="MARK_RAN">
+            <sqlCheck expectedResult="0">
+                select count(*) from FF4J_FEATURES where FEAT_UID =
+                '{{ffName}}'
+            </sqlCheck>
+        </preConditions>
+        <insert tableName="FF4J_FEATURES">
+            <column name="FEAT_UID" value="{{ffName}}"/>
+            <column name="ENABLE" value="0"/>
+            <column name="DESCRIPTION"
+                    value="{{ffDescription}}"/>
+            <column name="GROUPNAME" value="Sprint {{ffSprintNumber}}"/>
+        </insert>
+    </changeSet>
+
+    <changeSet author="{{ffAuthor}}" id="update_FF_{{ffNumber}}">
+        <preConditions onFail="MARK_RAN">
+            <not>
+                <sqlCheck expectedResult="0">
+                    select count(*) from FF4J_FEATURES where FEAT_UID =
+                    '{{ffName}}'
+                </sqlCheck>
+            </not>
+        </preConditions>
+        <update tableName="FF4J_FEATURES">
+            <column name="ENABLE" value="0"/>
+            <column name="DESCRIPTION"
+                    value="{{ffDescription}}"/>
+            <column name="STRATEGY" value="null"/>
+            <column name="EXPRESSION" value="null"/>
+            <column name="GROUPNAME" value="Sprint {{ffSprintNumber}}"/>
+            <where>FEAT_UID='{{ffName}}'</where>
+        </update>
+    </changeSet>
+
+    <changeSet author="{{ffAuthor}}" id="insert_FF_type_{{ffNumber}}">
+        <preConditions onFail="MARK_RAN">
+            <sqlCheck expectedResult="0">
+                select count(*) from FF4J_CUSTOM_PROPERTIES where FEAT_UID =
+                '{{ffName}}' and
+                PROPERTY_ID = 'type'
+            </sqlCheck>
+        </preConditions>
+        <insert tableName="FF4J_CUSTOM_PROPERTIES">
+            <column name="PROPERTY_ID" value="type"/>
+            <column name="CLAZZ" value="org.ff4j.property.PropertyString"/>
+            <column name="CURRENTVALUE" value="{{ffType}}"/>
+            <column name="DESCRIPTION" value=""/>
+            <column name="FEAT_UID" value="{{ffName}}"/>
+        </insert>
+    </changeSet>
---
+++ cli.js
@@ -0,0 +1,102 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const _ = require('underscore');
+const fs = require('fs');
+const Configstore = require('configstore');
+
+const config = new Configstore('ffgenStore');
+
+const intro = () => {
+    console.log(chalk.yellow(figlet.textSync('FF is Easy')));
+    console.log(chalk.gray('Generate Your ChangeSets'));
+
+    _.templateSettings = {
+        interpolate: /\{\{(.+?)\}\}/g
+    };
+};
+
+module.exports = async () => {
+    intro();
+    prompt();
+};
+
+async function prompt() {
+
+    const templateVars = {};
+
+    let ffAuthorStored = config.get('ffAuthor');
+    let ffAuthor = await inquirer.prompt({
+        type: 'input',
+        name: 'ffAuthor',
+        default: ffAuthorStored,
+        message: 'ChangeSet Author:',
+    });
+    templateVars['ffAuthor'] = ffAuthor['ffAuthor'];
+    config.set('ffAuthor', ffAuthor['ffAuthor']);
+
+    let ffSprintNumberStored = config.get('ffSprintNumber');
+    let ffSprintNumber = await inquirer.prompt({
+        type: 'input',
+        name: 'ffSprintNumber',
+        default: ffSprintNumberStored,
+        message: 'Sprint Number:',
+    });
+    templateVars['ffSprintNumber'] = ffSprintNumber['ffSprintNumber'];
+    config.set('ffSprintNumber', ffSprintNumber['ffSprintNumber']);
+
+    let ffName = await inquirer.prompt({
+        type: 'input',
+        name: 'ffName',
+        message: 'FF Name (example: SMRT-1111.xxx.enabled):',
+    });
+    templateVars['ffName'] = ffName['ffName'];
+
+    let ffDescription = await inquirer.prompt({
+        type: 'input',
+        name: 'ffDescription',
+        message: 'FF Description (example: SMRT-1111 Новая услуга)',
+    });
+    templateVars['ffDescription'] = ffDescription['ffDescription'];
+
+    let ffType = await inquirer.prompt({
+        type: 'list',
+        name: 'ffType',
+        message: 'Select FF Type:',
+        default: 'RELEASE',
+        choices: [{
+            name: 'RELEASE',
+            value: 'RELEASE'
+        }, {
+            name: 'SYSTEM',
+            value: 'SYSTEM'
+        }]
+    });
+    templateVars['ffType'] = ffType['ffType'];
+
+    let ffTag = await inquirer.prompt({
+        type: 'list',
+        name: 'ffTag',
+        message: 'Select FF Tag(optional):',
+        default: 'WEB',
+        choices: [{
+            name: 'WEB',
+            value: 'WEB'
+        }, {
+            name: 'TEST',
+            value: 'TEST'
+        }, {
+            name: 'MOBILE',
+            value: 'MOBILE'
+        }]
+    });
+    templateVars['ffTag'] = ffTag['ffTag'];
+    templateVars['ffNumber'] = ffName['ffName'].split('.')[0];
+
+    let templateBody = fs.readFileSync('./changeset.tpl', 'utf8');
+
+    let template = _.template(templateBody);
+    let out = template(templateVars);
+
+    fs.writeFileSync('changeSet.xml', out);
+};
